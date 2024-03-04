b7e5160ccadf7c4296af3420bdd14dfe353967c7
unknown <luca.buttini@iisviolamarchesini.edu.it>
2024-03-04 15:43:29 +0100

    prova

---
+++ ProvaDadoConsole/ProvaDadoConsole/Dado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaDadoConsole
+{
+    internal class Dado
+    {
+        int faccie, numero;
+        Random rnd;
+        public Dado(int _faccie)
+        {
+            rnd = new Random();
+            faccie = _faccie;
+            numero = rnd.Next(1, 7);
+        }
+        public int Faccie { get => faccie; set => faccie = value; }
+        public int Numero { get => numero; set => numero = value; }
+        public string ScriviDado()
+        {
+            return ($"Numero faccie: {Faccie}, numero: {Numero}");
+        }
+    }
+}
---
+++ ProvaDadoConsole/ProvaDadoConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaDadoConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dado dado = new Dado(4);
+            Console.WriteLine(dado.ScriviDado());
+            Console.ReadLine();
+        }
+    }
+}
