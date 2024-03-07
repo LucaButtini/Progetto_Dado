113f12270355da1d91c6199cc21dba2647095703
unknown <luca.buttini@iisviolamarchesini.edu.it>
2024-03-07 22:22:24 +0100

    inizio visuale

---
+++ GiocoDado/GiocoDado/Dado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiocoDado
+{
+    internal class Dado
+    {
+        int _facciaDado;
+
+        public int FacciaDado { get => _facciaDado; set => _facciaDado = value; }
+
+        public Dado()
+        {
+            _facciaDado = 0;
+        }
+        static public bool operator >(Dado dado1, Dado dado2)
+        {
+            return (dado1.FacciaDado > dado2.FacciaDado);
+        }
+        static public bool operator <(Dado dado1, Dado dado2)
+        {
+            return (dado1.FacciaDado < dado2.FacciaDado);
+        }
+        static public bool operator ==(Dado dado1, Dado dado2)
+        {
+            return dado1.FacciaDado == dado2.FacciaDado;
+        }
+        static public bool operator !=(Dado dado1, Dado dado2)
+        {
+            return !(dado1.FacciaDado == dado2.FacciaDado);
+        }
+    }
+}
